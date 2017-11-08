410e821ea63d3b4d8f3e078e233fb7a8ac5a77f4
chencx <345057113@qq.com>
2017-11-08 16:13:29 +0800

    支持G9/G19同时编译

--- runinstall.sh
+++ runinstall.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 ROOTFS_DIR=$1
+ROOTFS_DIR_G19=$2
 
 # 安装sgminer及其库(删除sgminer)
 make install
 rm -rf ${ROOTFS_DIR}/bin/sgminer
 
 cp innominer_* ${ROOTFS_DIR}/bin/
-
+mv innominer_* ${ROOTFS_DIR_G19}/bin/
