55979a8e2546607caee52ba8aeca52e7e98a11a9
PengPeng <pengp@innosilicon.com.cn>
2017-12-11 14:09:41 +0800

    使用inno_miner目录中编译出的so

--- runmake.sh
+++ runmake.sh
@@ -7,8 +7,8 @@ ROOTFS_DIR=$1
 MAKE_JOBS=$2
 ROOTFS_DIR_G19=$3
 
-LDFLAGS=-L${ROOTFS_DIR}/lib \
-CFLAGS=-I${ROOTFS_DIR}/include \
+LDFLAGS="-L${ROOTFS_DIR}/lib -linno " \
+CFLAGS="-I${ROOTFS_DIR}/include -Wall " \
 ./configure --prefix=${ROOTFS_DIR} \
 --enable-coinflex --without-curses --host=arm-xilinx-linux-gnueabi --build=x86_64-pc-linux-gnu #--target=arm
 
