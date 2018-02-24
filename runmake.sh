1f75d58c48b913dd9220c6fbfc21b3eea4b3ff09
PengPeng <pengp@innosilicon.com.cn>
2018-02-24 18:08:27 +0800

    完善编译脚本,修复编译出错

--- runmake.sh
+++ runmake.sh
@@ -3,18 +3,18 @@
 #make distclean
 ./autogen.sh
 
-#ROOTFS_DIR=$1
-ROOTFS_DIR=/home/public/duanh/src/rootfs
-#MAKE_JOBS=$2
-MAKE_JOBS=8
+ROOTFS_DIR=$1
+#ROOTFS_DIR=/home/public/duanh/src/rootfs
+MAKE_JOBS=$2
+#MAKE_JOBS=8
 ROOTFS_DIR_G19=$3
 
-LDFLAGS="-L${ROOTFS_DIR}/lib -linno -lim_lib -lim_drv " \
+LDFLAGS="-L${ROOTFS_DIR}/lib -lim_lib -lim_drv " \
 CFLAGS="-I${ROOTFS_DIR}/include -Wall " \
 ./configure --prefix=${ROOTFS_DIR} \
 --enable-coinflex --without-curses --host=arm-xilinx-linux-gnueabi --build=x86_64-pc-linux-gnu #--target=arm
 
 make -j${MAKE_JOBS}
 arm-xilinx-linux-gnueabi-strip sgminer
-cp sgminer innominer_T3
+cp sgminer innominer_D11
 
